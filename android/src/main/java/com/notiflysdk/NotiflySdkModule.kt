6a6023eeddd96574ff3eee331a26b644945e9c69
edenkim00 <eotjd0986@kaist.ac.kr>
2023-06-16 17:58:27 +0900

    2.3.4

--- android/src/main/java/com/notiflysdk/NotiflySdkModule.kt
+++ android/src/main/java/com/notiflysdk/NotiflySdkModule.kt
@@ -31,7 +31,7 @@ class NotiflySdkModule internal constructor(private val reactContext: ReactAppli
       Notifly.setSdkType(NotiflyControlTokenImpl(), NotiflySdkType.REACT_NATIVE)
       Notifly.setSdkVersion(
         NotiflyControlTokenImpl(),
-        "2.3.3"
+        "2.3.4"
       ) // TODO: get version from package.json
       Notifly.initialize(reactContext, projectId, username, password)
       promise.resolve(null)
