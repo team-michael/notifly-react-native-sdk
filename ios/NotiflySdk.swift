a8d1768c14a8a91d78928a782fd537c7e1817b80
csjunha <qkrwnsgk0303@kaist.ac.kr>
2023-08-14 13:06:27 +0900

    Bump version to 3.0.1

--- ios/NotiflySdk.swift
+++ ios/NotiflySdk.swift
@@ -11,7 +11,7 @@ class NotiflySdk: NSObject {
     @objc(initialize:withUsername:withPassword:withResolver:withRejecter:)
     func initialize(projectId: String, username: String, password: String, resolve: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
         Notifly.setSdkType(type: "react_native")
-        Notifly.setSdkVersion(version: "3.0.0") // TODO: get version from package.json
+        Notifly.setSdkVersion(version: "3.0.1") // TODO: get version from package.json
         Notifly.initialize(projectId: projectId, username: username, password: password)
         resolve(nil)
     }
