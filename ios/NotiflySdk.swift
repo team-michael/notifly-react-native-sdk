bbe258ce8a7dd318f3bae87a7521f4d4f5c9b661
daeseongKim <79857081+edenkim00@users.noreply.github.com>
2024-05-03 14:41:32 +0900

    chore: deploy 3.3.0 (#31)

--- ios/NotiflySdk.swift
+++ ios/NotiflySdk.swift
@@ -6,7 +6,7 @@ class NotiflyReactNativeSdk: NSObject {
     @objc(initialize:withUsername:withPassword:withResolver:withRejecter:)
     func initialize(projectId: String, username: String, password: String, resolve: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
         Notifly.setSdkType(type: "react_native")
-        Notifly.setSdkVersion(version: "3.2.0") // TODO: get version from package.json
+        Notifly.setSdkVersion(version: "3.3.0") // TODO: get version from package.json
         Notifly.initialize(projectId: projectId, username: username, password: password)
         resolve(nil)
     }
