1e8dabd6fa4f47acf3048dfb14587fa5a6da6bbb
daeseongKim <79857081+edenkim00@users.noreply.github.com>
2024-08-27 18:18:46 +0900

    chore: deploy 3.6.1 (#36)
    * chore: deploy 3.6.1
    
    * chore: deploy 3.6.1
    
    ---------
    
    Co-authored-by: Ai Ke <aike@Aiui-MacBookPro.local>

--- android/src/main/java/tech/notifly/rn/NotiflySdkModule.kt
+++ android/src/main/java/tech/notifly/rn/NotiflySdkModule.kt
@@ -35,7 +35,7 @@ class NotiflySdkModule internal constructor(private val reactContext: ReactAppli
       Notifly.setSdkType(NotiflyControlTokenImpl(), NotiflySdkWrapperType.REACT_NATIVE)
       Notifly.setSdkVersion(
         NotiflyControlTokenImpl(),
-        "3.6.0"
+        "3.6.1"
       )
 
       Notifly.initialize(context, projectId, username, password)
