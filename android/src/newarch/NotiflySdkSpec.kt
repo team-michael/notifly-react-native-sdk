6340c7753537f055871a94724fcd5ff5b60f3d9c
Minkyu Cho <minkyu@greyboxhq.com>
2025-03-28 17:58:43 +0900

    feat: update react native

--- android/src/newarch/NotiflySdkSpec.kt
+++ android/src/newarch/NotiflySdkSpec.kt
@@ -3,5 +3,5 @@ package tech.notifly.rn
 import com.facebook.react.bridge.ReactApplicationContext
 
 abstract class NotiflySdkSpec internal constructor(context: ReactApplicationContext) :
-  NativeNotiflySdkSpec(context) {
+  NativeNotiflyReactNativeSdkSpec(context) {
 }
