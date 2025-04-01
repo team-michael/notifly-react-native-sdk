52a0d14c1febccbfa8b03bd2d753714bf9864ba0
Minkyu Cho <minkyu@greyboxhq.com>
2025-04-01 17:00:24 +0900

    feat: update sdk version

--- example/ios/clean.sh
+++ example/ios/clean.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#pod cache clean notifly_sdk --all
-#rm -rf ~/Library/Caches/CocoaPods
-#rm -rf Pods
-#rm -rf Podfile.lock
-#sudo rm -rf ~/Library/Developer/Xcode/DerivedData/*
-#pod repo update
-rm -rf ~/Library/Caches/CocoaPods Pods ~/Library/Developer/Xcode/DerivedData/*; pod deintegrate; pod setup;
+rm -rf Pods Podfile.lock ~/Library/Caches/CocoaPods Pods ~/Library/Developer/Xcode/DerivedData/*
+pod cache clean notifly_sdk --all
+pod repo update
+pod deintegrate
+pod setup
 bundle install && RCT_NEW_ARCH_ENABLED=1 bundle exec pod install --repo-update
