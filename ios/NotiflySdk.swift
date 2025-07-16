58028fe01ea9d5be5dc1814eb3c4ec141312ddfe
Soomin Lee <soomin@greyboxhq.com>
2025-07-16 18:52:34 +0900

    feat: add template name to event params

--- ios/NotiflySdk.swift
+++ ios/NotiflySdk.swift
@@ -9,7 +9,7 @@ class NotiflyReactNativeSdk: NSObject {
     reject _: RCTPromiseRejectBlock
   ) {
     Notifly.setSdkType(type: "react_native")
-    Notifly.setSdkVersion(version: "3.10.0") // TODO: get version from package.json
+    Notifly.setSdkVersion(version: "3.10.1") // TODO: get version from package.json
     Notifly.initialize(projectId: projectId, username: username, password: password)
     resolve(nil)
   }
